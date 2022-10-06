59c005296ce1815a1d17aa84992e038586444a81
kpurbia <kunalpurbia1990@gmail.com>
2022-10-06 23:22:58 +0530

    Set routes and increase functionalities

---
+++ 00-FrontEnd/routes/vendorRoutes.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const publicPath = path.join(__dirname, "..", "public");
+
+module.exports = function(app){
+    app.get("/vendor", (req, res)=>{
+        res.sendFile(publicPath + "/homepage/vendorHome.html");
+    });
+}

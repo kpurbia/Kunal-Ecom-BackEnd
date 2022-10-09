b10f0d915f65f3904f698cbc789c7aa03ec155ea
kpurbia <kunalpurbia1990@gmail.com>
2022-10-09 15:36:08 +0530

    Product display for all type of user is done

--- 00-FrontEnd/public/scripts/app.js
+++ 00-FrontEnd/public/scripts/app.js
@@ -7,7 +7,24 @@ var productsDisplay = () => {
         url: checkURL,
         type: "GET",
         success: (data, status) => {
-            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                document.getElementById("card").style.visibility = "visible";
+                var title = document.createElement("h5");
+                title.classList.add("card-title", "me-3");
+                title.innerHTML = data[i].product_name
+
+                var description = document.createElement("p");
+                description.classList.add("card-text", "me-3");
+                description.innerHTML = data[i].product_description
+                
+                var detailButton = document.createElement("button");
+                detailButton.classList.add("btn", "btn-primary");
+                detailButton.innerHTML = "SEE DETAILS";
+
+                document.getElementById("cardBody").appendChild(title);
+                document.getElementById("cardBody").appendChild(description);
+                document.getElementById("cardBody").appendChild(detailButton);
+            }
         },
         error: (data, status) => {
             alert("You are not authenticated to access");
